f7f5414bc2b60e79dbed86eeb5880108e821c9e5
Yingzhe Cui <50771527+Cuiyingzhe@users.noreply.github.com>
2022-05-05 23:43:29 +0800

    Update spectrum.py

--- spectrum.py
+++ spectrum.py
@@ -20,7 +20,7 @@ def cspectrum(n,m,x,p):
 
     The original code is written by Dr. Li Jianping in Fortran77 on May 12, 1998. 
     http://lijianping.cn/dct/attach/Y2xiOmNsYjpmOjk4Mw== (retrieved on May 5, 2022)
-    This python version by CUI Yingzhe, May 5, 2022. Email: cuiyingzhe@stu.ouc.edu.cn
+    This python version is by CUI Yingzhe, May 5, 2022. Email: cuiyingzhe@stu.ouc.edu.cn
     '''
     sl = np.zeros((m+1))
     ol = np.zeros((m+1))
